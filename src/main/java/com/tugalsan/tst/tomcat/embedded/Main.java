cebcd24be3667e18c3964bab2303626abd5e0722
tugalsan karabacak <tugalsan@gmail.com>
2022-11-23 09:18:43 +0300

    tomcat dep increased

--- src/main/java/com/tugalsan/tst/tomcat/embedded/Main.java
+++ src/main/java/com/tugalsan/tst/tomcat/embedded/Main.java
@@ -9,13 +9,13 @@ public class Main {
     //C:\me\codes\com.tugalsan\tst\com.tugalsan.tst.tomcat.embedded>target\bin\webapp.bat
     public static void main(String[] args) {
         var tomcatBall = TS_Tomcat.of(
-                Main.class,
+                null/*Main.class*/,
                 "",
                 servlets -> {
                     servlets.add(new ServletByMapping());
                 }, connectors -> {
-                    connectors.add(TS_TomcatConnector.ofUnSecure(8085));
-                    connectors.add(TS_TomcatConnector.ofUnSecure(8086));
+                    connectors.add(TS_TomcatConnector.ofUnSecure(8087));
+//                    connectors.add(TS_TomcatConnector.ofUnSecure(8088));
                 }
         );
         System.out.println(tomcatBall);
