a4289e5836220634035e5a33b9393e9dd7c2f196
tugalsan karabacak <tugalsan@gmail.com>
2022-11-30 08:56:57 +0300

    simplified

--- src/main/java/com/tugalsan/tst/tomcat/embedded/Main.java
+++ src/main/java/com/tugalsan/tst/tomcat/embedded/Main.java
@@ -1,6 +1,7 @@
 package com.tugalsan.tst.tomcat.embedded;
 
-import com.tugalsan.api.tomcat.embedded.server.servlets.TS_ServletAliveByMapping;
+import com.tugalsan.api.tomcat.embedded.server.TS_TomcatBall;
+import com.tugalsan.api.tomcat.embedded.server.servlets.*;
 import com.tugalsan.api.tomcat.embedded.server.utils.*;
 
 public class Main {
@@ -8,7 +9,7 @@ public class Main {
     //HOW TO EXECUTE
     //C:\me\codes\com.tugalsan\tst\com.tugalsan.tst.tomcat.embedded>target\bin\webapp.bat
     public static void main(String[] args) {
-        var tomcatBall = com.tugalsan.api.tomcat.embedded.server.TS_TomcatBall.of("",
+        var tomcatBall = TS_TomcatBall.of("",
                 servlets -> {
                     servlets.add(new TS_ServletAliveByMapping());
                 }, connectors -> {
