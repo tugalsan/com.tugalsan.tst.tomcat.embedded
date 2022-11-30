25bbf145517869e0ff8df8eafbcc33a94ebcbef8
tugalsan karabacak <tugalsan@gmail.com>
2022-11-30 08:46:45 +0300

    porting to api

--- src/main/java/com/tugalsan/tst/tomcat/embedded/Main.java
+++ src/main/java/com/tugalsan/tst/tomcat/embedded/Main.java
@@ -1,13 +1,14 @@
 package com.tugalsan.tst.tomcat.embedded;
 
-import com.tugalsan.tst.tomcat.embedded.servlets.*;
+import com.tugalsan.api.tomcat.embedded.server.servlets.TS_ServletAliveByMapping;
+import com.tugalsan.api.tomcat.embedded.server.utils.*;
 
 public class Main {
 
     //HOW TO EXECUTE
     //C:\me\codes\com.tugalsan\tst\com.tugalsan.tst.tomcat.embedded>target\bin\webapp.bat
     public static void main(String[] args) {
-        var tomcatBall = TS_TomcatBall.of("",
+        var tomcatBall = com.tugalsan.api.tomcat.embedded.server.TS_TomcatBall.of("",
                 servlets -> {
                     servlets.add(new TS_ServletAliveByMapping());
                 }, connectors -> {
--- src/main/java/com/tugalsan/tst/tomcat/embedded/TS_ServletAbstract.java
+++
@@ -1,7 +0,0 @@
-package com.tugalsan.tst.tomcat.embedded;
-
-import jakarta.servlet.http.*;
-
-public abstract class TS_ServletAbstract extends HttpServlet{
-    abstract public String name();
-}
