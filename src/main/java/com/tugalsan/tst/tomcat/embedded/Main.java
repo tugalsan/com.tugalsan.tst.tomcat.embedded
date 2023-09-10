78c7ed7fac735e31e46580d2d6a25990a05da8b7
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-09-10 21:09:24 +0300

    Update Main.java

--- src/main/java/com/tugalsan/tst/tomcat/embedded/Main.java
+++ src/main/java/com/tugalsan/tst/tomcat/embedded/Main.java
@@ -18,4 +18,40 @@ public class Main {
         );
         System.out.println("SAFE EXIT: " + tomcatBall);
     }
+//
+//    public static void main(String... args)
+//            throws Exception {
+//        File baseFolder = new File(System.getProperty("user.dir"));
+//        File appsFolder = new File(baseFolder, "web-apps");
+//
+//        var tomcat = new Tomcat();
+//        tomcat.setBaseDir(baseFolder.getAbsolutePath());
+//        tomcat.setPort(8080);
+//        tomcat.getHost().setAppBase(appsFolder.getAbsolutePath());
+//
+//        // Call the connector to create the default connector.
+//        tomcat.getConnector();
+//
+//        tomcat.addWebapp("", appsFolder.getAbsolutePath());
+//        var wrapper = tomcat.addServlet("", "hello", new HelloServlet());
+//        wrapper.setLoadOnStartup(1);
+//        wrapper.addMapping("/*");
+//
+//        tomcat.start();
+//        tomcat.getServer().await();
+//    }
+//
+//    private static class HelloServlet extends HttpServlet {
+//
+//        @Override
+//        protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+//                throws IOException {
+//            resp.setStatus(200);
+//
+//            var writer = resp.getWriter();
+//            writer.write("Hello from Tomcat native image!");
+//            writer.flush();
+//            writer.close();
+//        }
+//    }
 }
